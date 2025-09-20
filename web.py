00edeeb729420a3f63184eaeb7926b7595c87e38
Bayram <91245046+Bayram13@users.noreply.github.com>
2025-09-20 13:55:06 +0400

    Update web.py

--- web.py
+++ web.py
@@ -1,17 +1,15 @@
-from flask import Flask
+import os
 import threading
-import bot  # sənin bot kodun (bot.py içindədir)
+from flask import Flask
+import bot  # yuxarıdakı bot.py faylı
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot is running on Render!"
-
-def run_bot():
-    bot.main()  # bot.py içində main funksiyan varsa onu çağır
+    return "✅ Bot is running on Render!"
 
 if __name__ == "__main__":
     # Botu ayrı thread-də işə salırıq
-    threading.Thread(target=run_bot).start()
+    threading.Thread(target=bot.main).start()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
